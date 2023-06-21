1f37edec5e072b8f7cc897c5ba371d80d0dfe04b
Piyush-Jadhav2804 <71430828+Piyush-Jadhav2804@users.noreply.github.com>
2023-06-21 17:13:52 +0530

    handled user's input

--- todo.js
+++ todo.js
@@ -1,5 +1,5 @@
-const tasks = []
-const taskList = document.getElementById('list');
+const tasks = [];
+const taskList = document.getElementById("list");
 const addTaskInput = document.getElementById("add");
 const tasksCounter = document.getElementById("tasks-counter");
 
@@ -11,4 +11,37 @@ function deleteTask(taskId) {}
 
 function addTask(task) {}
 
-function showNotification(text) {}
+function showNotification(text) {
+  alert(text);
+}
+
+//retreive the input given by user in input field
+function handleInputKeyPress(event) {
+  //if user pressed "Enter", we get the text by target.value
+  if(event.key === "Enter") {
+    const text = event.target.value;
+    console.log("text: ", text);
+
+    //if user haven't typed anything and just pressed "Enter", will give alert
+    if (!text) {
+      showNotification("Task text cannot be empty!!");
+      return;
+    }
+
+    //create "task" object
+    const task = {
+      text: text, //user input
+      id: Date.now().toString(), //whatever task is created, we have to assign an id
+      done: false, //whenever user is typing, we should set done as false
+    };
+
+    //once the user presses "Enter", we need to make input box empty after addding the text into list
+    event.target.value = "";
+    
+    //add this task into "task" array
+    addTask(task);
+  }
+}
+
+//used 'keyup' even, as we want the input given by user until he/she presses "Enter" button
+addTaskInput.addEventListener("keyup", handleInputKeyPress);
