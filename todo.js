aabceb98c26bb8ed4696e37a5d80cc622c64b8a4
Piyush-Jadhav2804 <71430828+Piyush-Jadhav2804@users.noreply.github.com>
2023-06-21 16:36:22 +0530

    Added UI for the project

---
+++ todo.js
@@ -0,0 +1,14 @@
+const tasks = []
+const taskList = document.getElementById('list');
+const addTaskInput = document.getElementById("add");
+const tasksCounter = document.getElementById("tasks-counter");
+
+function renderList() {}
+
+function markTaskAsComplete(taskId) {}
+
+function deleteTask(taskId) {}
+
+function addTask(task) {}
+
+function showNotification(text) {}
